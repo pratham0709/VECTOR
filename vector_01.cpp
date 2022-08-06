05ce072e3243a8c44ced3381449309d9d27b2ac8
pratham0709 <109234076+pratham0709@users.noreply.github.com>
2022-08-06 09:58:08 +0530

    Create vector_01.cpp

---
+++ vector_01.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	vector<int>v;
+	v.push_back(1); // {1}
+	v.push_back(8); // {1,8}
+	v.push_back(3); // {1,8,3}
+	cout<<v[1]<<endl;
+	int n = v.size();
+	cout<<n;
+}
+
+
+/* OUTPUT.....
+8
+3
+--------------------------------
+Process exited after 0.02551 seconds with return value 0
+Press any key to continue . . .
+
+*/
