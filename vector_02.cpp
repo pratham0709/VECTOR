eff40caed035b343de9b226b6109b67dc7b10f28
pratham0709 <109234076+pratham0709@users.noreply.github.com>
2022-08-06 13:56:54 +0530

    Update vector_02.cpp

--- vector_02.cpp
+++ vector_02.cpp
@@ -8,6 +8,7 @@ int main(){
 	v.push_back(3); // {1,8,3}
 	v.push_back(6); // {1,8,3,6}
 	v.push_back(9); // {1,8,3,6,9}
+	cout<< *(v.end()-1)<<endl;
 	
 	for(int i =0; i<v.size(); i++){
 		cout<<v[i]<<" " ;
@@ -16,6 +17,7 @@ int main(){
 
 
 /* OUTPUT........
+9
 1 8 3 6 9
 --------------------------------
 Process exited after 0.02188 seconds with return value 0
